f84e17a3212f40f93bdccec87077ff1d239d36e5
FreshFleshFlash <pharmacystic@gmail.com>
2015-09-15 15:08:32 +0900

    150915

--- asset/world-end.js
+++ asset/world-end.js
@@ -86,7 +86,7 @@ function getNextTweet() {
 
 function loadTweets(q, callback) {
 
-	twitter('search/tweets', {q: q||'world end', count: 50}, function(result){		
+	twitter('search/tweets', {q: q||'세상', count: 50}, function(result){		
 
 		var data = result.statuses;
 
@@ -155,39 +155,39 @@ function fillZero(num, cnt) {
 
 // twitter api 활용하기
 var twitterConfig = {	//==>왜 안되지??
-	baseUrl: "https://api.twitter.com/1.1/",
-	consumerKey: "SEWOpNtRl8aI6ICNqv8Cg",
-	consumerSecret: "LQ5H3W0ZC4UxTT3vuWHjp7GSUDaq0HWYQoMCxrOvo",
-	accessToken: "632490991-DicI7iYLX5kfGfVfgRPCvAQeDqkXLEAVTtBH9rLb",
-	tokenSecret: "dv6QQhv9Ku1rf8iqD3J1G75PnEsOjOQlM3MaF66WGdP5H"
+	baseUrl: 'https://api.twitter.com/1.1/',
+	consumerKey: 'SEWOpNtRl8aI6ICNqv8Cg',
+	consumerSecret: 'LQ5H3W0ZC4UxTT3vuWHjp7GSUDaq0HWYQoMCxrOvo',
+	accessToken: '632490991-DicI7iYLX5kfGfVfgRPCvAQeDqkXLEAVTtBH9rLb',
+	tokenSecret: 'dv6QQhv9Ku1rf8iqD3J1G75PnEsOjOQlM3MaF66WGdP5H'
 };
 
 function twitter(api, params, callback) {
 	
-	if (!api.match(/\.json$/)) api += ".json";
+	if (!api.match(/\.json$/)) api += '.json';
 
 	// 파라미터 기본세팅
-	params.oauth_version = "1.0";
-	params.oauth_signature_method = "HMAC-SHA1";
-	params.oauth_consumer_key = "SEWOpNtRl8aI6ICNqv8Cg";//twitterConfig.consumerKey; 
-	params.oauth_token = "632490991-DicI7iYLX5kfGfVfgRPCvAQeDqkXLEAVTtBH9rLb";//twitterConfig.accessToken;
+	params.oauth_version = '1.0';
+	params.oauth_signature_method = 'HMAC-SHA1';
+	params.oauth_consumer_key = 'SEWOpNtRl8aI6ICNqv8Cg';//twitterConfig.consumerKey; 
+	params.oauth_token = '632490991-DicI7iYLX5kfGfVfgRPCvAQeDqkXLEAVTtBH9rLb';//twitterConfig.accessToken;
 	
 	if (!params.callback && callback) { // callback을 직접 지정하지 않고 무기명 함수로 줄 경우 자동 생성한다.
-		params.callback = "ssh"+(Math.random()+"").replace("0.","");
+		params.callback = 'ssh'+(Math.random()+'').replace('0.','');
 		window[params.callback] = callback;
 	}
 	
 	var oauthMessage = {
-		method: "GET",
-		action: "https://api.twitter.com/1.1/" + api,//twitterConfig.baseUrl+api,
+		method: 'GET',
+		action: 'https://api.twitter.com/1.1/' + api,//twitterConfig.baseUrl+api,
 		parameters: params
 	};
 
 	// Oauth 인증관련
 	OAuth.setTimestampAndNonce(oauthMessage);
 	OAuth.SignatureMethod.sign(oauthMessage, {
-		consumerSecret: "LQ5H3W0ZC4UxTT3vuWHjp7GSUDaq0HWYQoMCxrOvo",//twitterConfig.consumerSecret,
-		tokenSecret: "dv6QQhv9Ku1rf8iqD3J1G75PnEsOjOQlM3MaF66WGdP5H"//twitterConfig.tokenSecret
+		consumerSecret: 'LQ5H3W0ZC4UxTT3vuWHjp7GSUDaq0HWYQoMCxrOvo',//twitterConfig.consumerSecret,
+		tokenSecret: 'dv6QQhv9Ku1rf8iqD3J1G75PnEsOjOQlM3MaF66WGdP5H'//twitterConfig.tokenSecret
 	});
 
 	// Oauth 인증하여 URL리턴(json type)
@@ -195,7 +195,7 @@ function twitter(api, params, callback) {
 	$.ajax({
 		type: oauthMessage.method,
 		url: jsonUrl,
-		dataType: "jsonp",
+		dataType: 'jsonp',
 		jsonp: false,
 		cache: true
 	}).fail(function(xhr){});
